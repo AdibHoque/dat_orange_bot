5504448810c41f9ade2fc2a45bfe2e03073c87e2
Bravo Zulu <36680137+granthood@users.noreply.github.com>
2018-05-02 00:19:26 -0500

    Trying to fix the bot status message

--- bot.py
+++ bot.py
@@ -13,8 +13,6 @@ async def on_ready():
     while True:
         await bot.change_presence(activity=discord.Game(name='bz.help with my friends!'))
         await asyncio.sleep(10)
-        await bot.change_presence(activity=discord.Game('with dat banana bot'))
-        await asyncio.sleep(10)
         await bot.change_presence(activity=discord.Game(f'with {len(client.guilds)} servers'))
         await asyncio.sleep(10)
 
