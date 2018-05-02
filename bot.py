c2d314d6fa3690d00e79e370f473f04f91b06b71
Bravo Zulu <36680137+granthood@users.noreply.github.com>
2018-05-02 00:04:03 -0500

    Fixed the status message bug
    Couldn't get the status message to work. Fixed now. New features coming soon!

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_ready():
         await asyncio.sleep(10)
         await bot.change_presence(activity=discord.Game('with dat banana bot'))
         await asyncio.sleep(5)
-        await bot.change_presence(activity=discord.Game('with {client.Guilds} servers'))
+        await bot.change_presence(activity=discord.Game(f'with {len(client.guilds)} servers'))
 
 @bot.event
 async def on_command_error(ctx, error):
