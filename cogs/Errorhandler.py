92e52dc0c9cc4d7366517b16c4c68b6a1226d24c
ur-mums-a-trap21 <35691890+ur-mums-a-trap21@users.noreply.github.com>
2018-05-02 20:49:13 -0400

    Update Errorhandler.py

--- cogs/Errorhandler.py
+++ cogs/Errorhandler.py
@@ -25,14 +25,15 @@ class ErrorHandler:
                 embed = discord.Embed(title = "Warning - Command Spam Detected :rage:!",description = "**"+ ctx.author.name +"**, Kindly wait for the command to cooldown", color = 0xFF0000)
                 embed.set_author(name=f"{ctx.author.display_name}", icon_url=f"{ctx.author.avatar_url}")
                 embed.add_field(name="Command Used: ", value = ctx.message.content)
-                await ctx.send(embed = embed)            elif isinstance (error, commands.MissingPermissions):
+                await ctx.send(embed = embed)            
+            elif isinstance (error, commands.MissingPermissions):
                 embed = discord.Embed(title = "Error - Missing Permissions!",description = "**"+ ctx.author.name +"**, you're not allowed to do this!", color =  0xFF0000)
                 embed.set_author(name=f"{ctx.author.display_name}", icon_url=f"{ctx.author.avatar_url}")
                 embed.add_field(name="Command Used: ",value = ctx.message.content)
                 await ctx.send(embed = embed)
 
             elif isinstance(error, commands.NotOwner):
-                embed = discord.Embed(title = "Error - Not my daddy! :smirk:",description = "**"+ ctx.author.name +"**, you think you're a smartass huh? :smirk:", color = 0xFF0000)
+                embed = discord.Embed(title = "Error - Not my Owner! :smirk:",description = "**"+ ctx.author.name +"**, you think you're smart huh? :smirk:", color = 0xFF0000)
                 embed.set_author(name=f"{ctx.author.display_name}", icon_url=f"{ctx.author.avatar_url}")
                 await ctx.send(embed = embed)
             try:
