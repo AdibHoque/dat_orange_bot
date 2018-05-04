53b32720284647ef5c947db312aa1a6826b9dfa9
ur-mums-a-trap21 <35691890+ur-mums-a-trap21@users.noreply.github.com>
2018-05-03 20:42:33 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -139,23 +139,6 @@ async def restart(ctx):
         os.execv(sys.executable, ['python'] + ['\\Users\\megat\\Desktop\\bravo_zulu\\bot.py'])
 
 
-@bot.command(pass_context=True, aliases=["clear"])
-@commands.has_permissions(manage_messages=True)
-async def purge(ctx, amount, description="Clears messages from the chat"):
-    try:
-        x = int(amount)
-        if x < 2 or x > 500:
-            return await ctx.send("Must be in range of 2 to 500 messages.")
-        await ctx.channel.purge(limit=x)
-        await ctx.send("Cleared {} messages for you. Emjoy the clear chat!", delete_after=5)
-    except ValueError:
-        await ctx.send("Please select a number.")
-    except commands.errors.MissingPermissions:
-        await ctx.send(on_command_error)
-    except Exception as e:
-        await ctx.send("Something went wrong. Here it is: {}".format(e))
-        print(e)
-
 @bot.command()
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, user: discord.Member):
