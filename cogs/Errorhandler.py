6725478f422a76e716d9b8a99e9d830984f9e8e5
Bravo Zulu <36680137+granthood@users.noreply.github.com>
2018-05-06 22:21:29 -0500

    Update Errorhandler.py

--- cogs/Errorhandler.py
+++ cogs/Errorhandler.py
@@ -10,7 +10,7 @@ class ErrorHandler:
     async def on_command_error(self, ctx, error):
             if isinstance (error, commands.CommandNotFound):
                 embed = discord.Embed(title = "Error - Command not found!",
-                                      description = "**" + ctx.author.name + "**, to view all my commands use: b.help", color = 0xFF0000)
+                                      description = "**" + ctx.author.name + "**, to view all my commands use: o.help", color = 0xFF0000)
                 embed.set_author(name=f"{ctx.author.display_name}", icon_url=f"{ctx.author.avatar_url}")
                 embed.add_field(name="Command Used: ", value = ctx.message.content)
                 await ctx.send(embed = embed)
