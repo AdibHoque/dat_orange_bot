d4e66c0182c2fbd76bf376513ed48a783e9d4353
Bravo Zulu <36680137+granthood@users.noreply.github.com>
2018-05-09 08:54:07 -0500

    fixed cogs.info

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ bot.load_extension("cogs.fun")
 bot.load_extension("cogs.pubg")
 bot.load_extension("cogs.math")
 bot.load_extension("cogs.idiotic")
-bot.load_extension("cogs.pokedex")
+bot.load_extension("cogs.info")
     
 if 'TOKEN' in os.environ:
     heroku = True
