59a82b0a728f6b141e9c33ab5b9aa327ca199060
Bravo Zulu <36680137+granthood@users.noreply.github.com>
2018-05-10 08:28:54 -0500

    added hype cmd

--- cogs/fun.py
+++ cogs/fun.py
@@ -255,5 +255,20 @@ class Fun:
     ─▄▀░░░▄▀░░░░░░░░▀▀▀▀█▀
     ▀░░░▄▀░░░░░░░░░░▀░░░▀▀▀▀▄▄▄▄▄""")
 
+    @commands.command(aliases=['hypu', 'train'])
+    async def hype(self, ctx):
+        '''HYPE TRAIN CHOO CHOO'''
+        hypu = ['https://cdn.discordapp.com/attachments/102817255661772800/219514281136357376/tumblr_nr6ndeEpus1u21ng6o1_540.gif',
+                'https://cdn.discordapp.com/attachments/102817255661772800/219518372839161859/tumblr_n1h2afSbCu1ttmhgqo1_500.gif',
+                'https://gfycat.com/HairyFloweryBarebirdbat',
+                'https://i.imgur.com/PFAQSLA.gif',
+                'https://abload.de/img/ezgif-32008219442iq0i.gif',
+                'https://i.imgur.com/vOVwq5o.jpg',
+                'https://i.imgur.com/Ki12X4j.jpg',
+                'https://media.giphy.com/media/b1o4elYH8Tqjm/giphy.gif']
+        msg = f':train2: CHOO CHOO {random.choice(hypu)}'
+        await ctx.send(msg)    
+        
+        
 def setup(bot):
     bot.add_cog(Fun(bot))
