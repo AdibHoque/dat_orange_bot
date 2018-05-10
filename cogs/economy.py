2a96f8379c4d64d35a9103d491ba036ca832bed0
Bravo Zulu <36680137+granthood@users.noreply.github.com>
2018-05-10 08:56:28 -0500

    Update economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -19,19 +19,27 @@ class Economy:
         self.dbl = x['dblapi']
 
     async def add_points(self, user, points):
-        x = await self.db.datbananabot.economy.find_one({"user": user.id})
+        x = await self.db.datorangebot.economy.find_one({"user": user.id})
         total = int(x['points']) + points
-        await self.db.datbananabot.economy.update_one({"user": user.id}, {"$set": {"points": total}}, upsert=True)
+        await self.db.datorangebot.economy.update_one({"user": user.id}, {"$set": {"points": total}}, upsert=True)
         
 
     async def is_registered(self, user):
-        x = await self.db.datbananabot.economy.find_one({"user": user.id})
+        x = await self.db.datorangebot.economy.find_one({"user": user.id})
         if x is None:
             return False
         else:
             return True
             
-            
+    
+    @commands.command(aliases=['register', 'openbank'])
+    async def openaccount(self, ctx):
+        '''Opens a bank account for the economy!'''
+        registered = await self.is_registered(ctx.author)
+        if registered is True:
+            return await ctx.send(f"You already have a bank account!")
+        await self.db.datorangebot.economy.update_one({"user": ctx.author.id}, {"$set": {"points": 0}}, upsert=True)
+            await ctx.send("B R O! you already have a bank account oml smh.")
             
             
 def setup(bot):
